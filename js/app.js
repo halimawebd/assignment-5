6383928c85f489dbdb4af0fd1a06646572156c52
halimawebd <halimawebd@gmail.com>
2022-08-30 19:15:16 +0600

    select name added

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const cartArray = [];
 function display(cartselect)
 {
 // console.log(cartselect)
+const cardBody = document.getElementById('cart-select');
 for(let i = 0; i < cartselect; i++)
 {
   console.log(cartArray[1]);
@@ -17,10 +18,23 @@ function addToCart(element)
   // console.log(element.parentNode.parentNode.children)
   // console.log(element.parentNode.parentNode.children[0].innerText)
 //  console.log(element.parentNode.children[0]);
-
+  
   const selectName = element.parentNode.children[0].innerText;
   console.log(selectName)
 
+  const listName = document.getElementById('list-name');
+  if(listName.children.length<5){
+    const li = document.createElement('li');
+    li.innerText = selectName;
+    listName.appendChild(li)
+   
+    element.setAttribute('disabled',true)
+  }
+  else{
+    alert('code')
+  }
+ 
+  console.log(element)
     const Selectobj ={
     selectName: selectName,
 
@@ -36,3 +50,16 @@ function addToCart(element)
 
   
 }
+
+// calculator-part
+
+// document.getElementById('total-calculate').addEventListener('click',function(){
+
+
+//   let playerExpenseElement = document.getElementById('Player Expenses');
+//   let playerExpenseElementString = playerExpenseElement.innerText;
+//   let playerExpense = parseInt( playerExpenseElementString);
+
+//   const payToManagerElement = getElementById('payToManager');
+//   const payToManagerElementString = payToManagerElement.value;
+// })
